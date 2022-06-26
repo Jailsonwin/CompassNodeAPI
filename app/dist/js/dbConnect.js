d6519262a6ab4b03a787fe8b092ddb847101a12b
Jailson Eduardo Ferens <75101885+Jailsonwin@users.noreply.github.com>
2022-06-26 13:26:30 -0300

    delete configuration file

--- app/dist/js/dbConnect.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-mongoose_1.default.connect("mongodb+srv://jailsonwin:laduo2001@cluster0.ic2hf.mongodb.net/compass-project");
-let db = mongoose_1.default.connection;
-exports.default = db;
