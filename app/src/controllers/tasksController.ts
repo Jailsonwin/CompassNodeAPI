e626c921398f80a338e1731977b8acdfceca386a
JoaogustavoKoche <joaogustavokoche@gmail.com>
2022-06-26 15:00:14 -0300

    Create user model

---
+++ app/src/controllers/tasksController.ts
@@ -0,0 +1,63 @@
+import tasks from "../models/Task.js";
+
+class TaskController {
+   static listTasks = (req, res) => {
+       tasks.find()
+           .populate('user')
+           .exec((err, tasks) => {
+               res.status(200).json(tasks);
+       });
+   }
+
+   static listTaskById = (req, res) => {
+       const id = req.params.id;
+
+       tasks.findById(id)
+           .populate('user')
+           .exec((err, tasks) => {
+               if(err) {
+                   res.status(404).send({message: `${err.message} - Task not found!`});
+               } else {
+                   res.status(200).send(tasks);
+               }
+       });
+   }
+
+   static addTask = (req, res) => {
+       let task = new tasks(req.body);
+
+       task.save((err: Error) => {
+           if(err) {
+               res.status(404).send({message: `${err.message} - Something went wrong, the Task has not been added!`});
+           } else {
+               res.status(201).send(task.toJSON());
+           }
+       });
+   }
+
+   static updateTask = (req, res) => {
+       const id = req.params.id;
+
+       tasks.findByIdAndUpdate(id, {$set: req.body}, (err: Error) => {
+           if(!err) {
+               res.status(200).send({message: "Task successfully updated!"});
+           } else {
+               res.status(404).send({message: err.message});
+           }
+       });
+   }
+
+   static deleteTasks = (req, res) => {
+       const id = req.params.id;
+
+       tasks.findByIdAndDelete(id, (err: Error) => {
+           if(!err) {
+               res.status(204).send({message: "Task sucessfully deleted!"});
+           } else {
+               res.status(404).send({message: err.message});
+           }
+       });
+   }
+}
+
+export default TaskController;
---
+++ app/src/routes/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import users from "./usersRoutes.js";
+import tasks from "./tasksRoutes.js";
+import { Express } from "express-serve-static-core";
+
+const routes = (app: Express) => {
+   app.route("/").get((req, res) => {
+       res.status(200).send({name: "REST API - PB React"});
+   });
+
+   app.use(express.json(), users, tasks);
+};
+
+export default routes;
