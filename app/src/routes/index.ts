25697c107d3a66e7518047243cf700f0b864991a
Jailsonwin <jailson.ferens@aluno.unc.br>
2022-06-26 14:21:54 -0300

    Creating Tasks routes

---
+++ app/src/routes/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import users from "./usersRoutes.js";
+import tasks from "./tasksRoutes.js";
+import { Express } from "express-serve-static-core";
+
+const routes = (app: Express) => {
+    app.route("/").get((req, res) => {
+        res.status(200).send({name: "REST API"});
+    });
+    app.use(express.json(), users, tasks);
+};
+
+export default routes;
