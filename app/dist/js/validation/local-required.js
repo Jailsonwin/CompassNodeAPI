5267e16c531d01c04a291f60ae42812dc4385179
JoaogustavoKoche <joaogustavokoche@gmail.com>
2022-06-28 03:05:01 -0300

    add required complement, number, and address

--- app/dist/js/validation/local-required.js
+++ app/dist/js/validation/local-required.js
@@ -3,10 +3,49 @@ const inputZip = document.getElementById('zipCode');
 const inputCountry = document.getElementById('country');
 const inputState = document.getElementById('state');
 const inputCity = document.getElementById('city');
+const inputComplement = document.getElementById('complement');
+const inputNumber = document.getElementById('number');
+const inputAddress = document.getElementById('address');
 inputZip.addEventListener("change", validateZip);
 inputCountry.addEventListener("change", validateCountry);
 inputState.addEventListener("change", validateState);
 inputCity.addEventListener("change", validateCity);
+inputComplement.addEventListener("change", validateComplement);
+inputNumber.addEventListener("change", validateNumber);
+inputAddress.addEventListener("change", validateAddress);
+function validateComplement() {
+    const complement = document.querySelector('#complement');
+    var erroComplement = document.querySelector("#erroComplement");
+    if (complement.value == '') {
+        complement.classList.add("errorInput");
+        erroComplement.classList.remove("invisible");
+    }
+    else {
+        complement.classList.remove("errorInput");
+    }
+}
+function validateNumber() {
+    const number = document.querySelector('#number');
+    var erroNumber = document.querySelector("#erroNumber");
+    if (number.value == '') {
+        number.classList.add("errorInput");
+        erroNumber.classList.remove("invisible");
+    }
+    else {
+        number.classList.remove("errorInput");
+    }
+}
+function validateAddress() {
+    const address = document.querySelector('#address');
+    var erroAddress = document.querySelector("#erroAddress");
+    if (address.value == '') {
+        address.classList.add("errorInput");
+        erroAddress.classList.remove("invisible");
+    }
+    else {
+        address.classList.remove("errorInput");
+    }
+}
 function validateState() {
     const state = document.querySelector('#state');
     var erroState = document.querySelector("#erroState");
--- app/src/validation/local-required.ts
+++ app/src/validation/local-required.ts
@@ -2,14 +2,57 @@ const inputZip = document.getElementById('zipCode') as HTMLElement;
 const inputCountry = document.getElementById('country') as HTMLElement;
 const inputState = document.getElementById('state') as HTMLElement;
 const inputCity = document.getElementById('city') as HTMLElement;
+const inputComplement = document.getElementById('complement') as HTMLElement;
+const inputNumber = document.getElementById('number') as HTMLElement;
+const inputAddress = document.getElementById('address') as HTMLElement;
 
 inputZip.addEventListener("change", validateZip);
 inputCountry.addEventListener("change", validateCountry);
 inputState.addEventListener("change", validateState);
 inputCity.addEventListener("change", validateCity);
+inputComplement.addEventListener("change", validateComplement);
+inputNumber.addEventListener("change", validateNumber);
+inputAddress.addEventListener("change", validateAddress);
 
 
+function validateComplement(){
 
+    const complement = document.querySelector('#complement') as HTMLInputElement;
+    var erroComplement = document.querySelector("#erroComplement") as HTMLElement;
+
+    if(complement.value == ''){
+        complement.classList.add("errorInput")
+        erroComplement.classList.remove("invisible")
+    }else{
+        complement.classList.remove("errorInput")
+    }
+}
+
+function validateNumber(){
+
+    const number = document.querySelector('#number') as HTMLInputElement;
+    var erroNumber = document.querySelector("#erroNumber") as HTMLElement;
+
+    if(number.value == ''){
+        number.classList.add("errorInput")
+        erroNumber.classList.remove("invisible")
+    }else{
+        number.classList.remove("errorInput")
+    }
+}
+
+function validateAddress(){
+
+    const address = document.querySelector('#address') as HTMLInputElement;
+    var erroAddress = document.querySelector("#erroAddress") as HTMLElement;
+
+    if(address.value == ''){
+        address.classList.add("errorInput")
+        erroAddress.classList.remove("invisible")
+    }else{
+        address.classList.remove("errorInput")
+    }
+}
 
 function validateState(){
 
