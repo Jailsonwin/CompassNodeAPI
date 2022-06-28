76ac6386d8f99e221d858d5ffd26218ccf58a063
JoaogustavoKoche <joaogustavokoche@gmail.com>
2022-06-28 02:55:03 -0300

    add required state and city

--- app/dist/js/validation/local-required.js
+++ app/dist/js/validation/local-required.js
@@ -1,8 +1,34 @@
 "use strict";
 const inputZip = document.getElementById('zipCode');
 const inputCountry = document.getElementById('country');
+const inputState = document.getElementById('state');
+const inputCity = document.getElementById('city');
 inputZip.addEventListener("change", validateZip);
 inputCountry.addEventListener("change", validateCountry);
+inputState.addEventListener("change", validateState);
+inputCity.addEventListener("change", validateCity);
+function validateState() {
+    const state = document.querySelector('#state');
+    var erroState = document.querySelector("#erroState");
+    if (state.value == '') {
+        state.classList.add("errorInput");
+        erroState.classList.remove("invisible");
+    }
+    else {
+        state.classList.remove("errorInput");
+    }
+}
+function validateCity() {
+    const city = document.querySelector('#city');
+    var erroCity = document.querySelector("#erroCity");
+    if (city.value == '') {
+        city.classList.add("errorInput");
+        erroCity.classList.remove("invisible");
+    }
+    else {
+        city.classList.remove("errorInput");
+    }
+}
 function validateZip() {
     const zip = document.querySelector('#zipCode');
     var erroZip = document.querySelector("#erroZip");
--- app/src/validation/local-required.ts
+++ app/src/validation/local-required.ts
@@ -1,9 +1,41 @@
 const inputZip = document.getElementById('zipCode') as HTMLElement;
 const inputCountry = document.getElementById('country') as HTMLElement;
+const inputState = document.getElementById('state') as HTMLElement;
+const inputCity = document.getElementById('city') as HTMLElement;
 
 inputZip.addEventListener("change", validateZip);
 inputCountry.addEventListener("change", validateCountry);
+inputState.addEventListener("change", validateState);
+inputCity.addEventListener("change", validateCity);
 
+
+
+
+function validateState(){
+
+    const state = document.querySelector('#state') as HTMLInputElement;
+    var erroState = document.querySelector("#erroState") as HTMLElement;
+
+    if(state.value == ''){
+        state.classList.add("errorInput")
+        erroState.classList.remove("invisible")
+    }else{
+        state.classList.remove("errorInput")
+    }
+}
+
+function validateCity(){
+
+    const city = document.querySelector('#city') as HTMLInputElement;
+    var erroCity = document.querySelector("#erroCity") as HTMLElement;
+
+    if(city.value == ''){
+        city.classList.add("errorInput")
+        erroCity.classList.remove("invisible")
+    }else{
+        city.classList.remove("errorInput")
+    }
+}
 
 function validateZip(){
 
